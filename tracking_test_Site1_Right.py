aee5636bb7cf1ae532a6b03f272952d17d710afc
박병규 <pbk5485@gmail.com>
2024-06-10 15:47:37 +0900

    Update tracking_test_Site1_Right.py

--- tracking_test_Site1_Right.py
+++ tracking_test_Site1_Right.py
@@ -126,7 +126,8 @@ def main(args):
 
         # save result
         dt = dt[0]
-        gt_data = pd.read_excel("gt/Site1_R_10_Trial6.xlsx")
+        # gt_data = pd.read_excel("gt/Site1_R_10_Trial6.xlsx")
+        gt_data = pd.read_excel(args.input[:-3] + 'xlsx') # gt path
         gt_times = gt_data['time'].values
         gt_x_positions = gt_data['ground_truth'].values
         gt = np.column_stack((gt_times, gt_x_positions))
