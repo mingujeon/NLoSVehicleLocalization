4455c05c09e562f99100289ffd01ba42769552a9
박병규 <pbk5485@gmail.com>
2024-06-14 01:01:37 +0900

    Update featureExtract.sh

--- featureExtract.sh
+++ featureExtract.sh
@@ -1,4 +1,4 @@
-FILES=$(find cls_features/SA/Dynamic3/Dynamic3_Site1_L_10_Trial9_1 -name "*.wav")
+FILES=$(find ARILDataset/ -name "*.wav")
 for i in $FILES ; do
   echo $i
   python featureExtract.py --input=$i
